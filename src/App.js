d04ab7f26d36b60a3f7bf7caf98d526604feeb7f
Nemitha Wijerathna <nemithan05@gmail.com>
2024-06-23 17:18:08 +0530

    basic navbar creation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from 'react'
+import './App.css'
 
-import Article from './components/article/Article'
 import Navbar from './components/navbar/Navbar'
-import Feature from './components/feature/Feature'
 import Brand from './components/brand/Brand'
 import CTA from './components/cta/CTA'
 
+import Blog from './containers/blog/Blog'
+import Features from './containers/features/Features'
+import Footer from './containers/footer/Footer'
+import Header from './containers/header/Header'
+import Possibility from './containers/possibility/Possibility'
+import WhatGPT3 from './containers/whatGPT3/WhatGPT3'
+
 const App = () => {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
