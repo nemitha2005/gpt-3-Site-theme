088c59b6aca0c9ccce37a642aef28bfec7ee99c9
Nemitha Wijerathna <nemithan05@gmail.com>
2024-06-23 13:19:12 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Article from './components/article/Article'
+import Navbar from './components/navbar/Navbar'
+import Feature from './components/feature/Feature'
+import Brand from './components/brand/Brand'
+import CTA from './components/cta/CTA'
+
+const App = () => {
+  return (
+    <div>
+        <h1>GPT-3</h1>
+    </div>
+  )
+}
+
+export default App
